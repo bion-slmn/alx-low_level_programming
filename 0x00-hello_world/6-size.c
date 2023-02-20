739cb66c80d1618544ebff5841f3fa243239c2d7
Bion Solomon <bionsol25@gmail.com>
2023-02-20 03:20:11 -0800

    siz 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,11 @@ int main(void)
 	long int tall;
 	float swim;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(bett));
-	printf("Size of an int: %lu byte(s)\n", sizeof(mbers));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(tall));
-	printf("Size of a long long int %lu byte(S)\n", sizeof(mref));
-	printf("Size of a float: %lu byte(s)\n", sizeof(swim));
+	printf("Size of a char: %ld byte(s)\n", sizeof(bett));
+	printf("Size of an int: %ld byte(s)\n", sizeof(mbers));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(tall));
+	printf("Size of a long long int %ld byte(S)\n", sizeof(mref));
+	printf("Size of a float: %ld byte(s)\n", sizeof(swim));
 	return (0);
 }
+
