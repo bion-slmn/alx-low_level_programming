ff5257c8a23b3d85c8291a4ae5a3821e38beec6c
Bion Solomon <bionsol25@gmail.com>
2023-02-20 01:04:59 -0800

    we goo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,11 +16,11 @@ int main(void)
 		{
 			for (num3 = num2 + 1; num3 <= 9; num3++)
 			{
-				putchar(num1 + '0');
-				putchar(num2 + '0');
-				putchar(num3 + '0');
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
 
-				if (num3 == 9 && num2 == 8 && num1 == 1)
+				if (num3 == 9 && num2 == 8 && num1 == 7)
 					continue;
 
 				putchar(',');
