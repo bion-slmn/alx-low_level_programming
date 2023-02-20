7163586abeeb7a1c1d33597cbc15566b14804855
Bion Solomon <bionsol25@gmail.com>
2023-02-20 03:36:39 -0800

    size 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,11 @@
  */
 int main(void)
 {
-	char bett;
-	int mbers;
-	long long int mref;
-	long int tall;
-	float swim;
-
-	printf("Size of a char: %ld byte(s)\n", sizeof(bett));
-	printf("Size of an int: %ld byte(s)\n", sizeof(mbers));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(tall));
-	printf("Size of a long long int %ld byte(S)\n", sizeof(mref));
-	printf("Size of a float: %ld byte(s)\n", sizeof(swim));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int %ld byte(S)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
 
