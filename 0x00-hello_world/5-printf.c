f69e188a08f9297bc6da209dd296e42ad7179c2f
Bion Solomon <bionsol25@gmail.com>
2023-02-20 02:35:49 -0800

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-int main(void) 
+/**
+ * main - PrintsHello printf.
+ * Return: 0  if success.
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 
 }
