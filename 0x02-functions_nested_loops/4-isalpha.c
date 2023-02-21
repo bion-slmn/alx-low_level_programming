db8cb5b019583194612dd7fbe24b79d50244c3bd
Bion Solomon <bionsol25@gmail.com>
2023-02-21 10:48:21 -0800

    is allpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for the alphabets
+ * @c: character to be checked
+ *
+ * Return: 1 if character is a letter,0 otherwise
+ */
+int _isalpha(int c);
+{
+	if (c >= 97 && c <= 122)
+	{
+		if (c >= 65 && c <= 90)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
