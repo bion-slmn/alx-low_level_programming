37565a491cfd0294e3b7027f64e11f9b1630515b
Bion Solomon <bionsol25@gmail.com>
2023-02-21 06:43:45 -0800

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = a; x < z; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+
+	return (0);
+}
