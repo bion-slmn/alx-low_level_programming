2c3bd0e0bb38d69ee445a6863dc54214144f4d8c
Bion Solomon <bionsol25@gmail.com>
2023-02-21 13:17:49 -0800

    24 hours jack

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+~include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the(24 hours) day
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 00; h <= 23; h++)
+	{
+		for (m = 00; m <= 59; m++)
+		{
+			_putchar((h \ 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m \ 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
