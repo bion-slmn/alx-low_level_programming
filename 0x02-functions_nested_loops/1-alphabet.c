8241904ddb877a85cf911ff5971dda12a78fb661
Bion Solomon <bionsol25@gmail.com>
2023-02-21 06:58:40 -0800

    i have insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,13 @@
 /**
  * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	char x;
 
-	for (x = 'a'; x < 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		_putchar(x);
 	}
