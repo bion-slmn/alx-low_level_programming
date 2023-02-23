670e89a2232a9b0ba63cdc8a10ea4a5fcf943fe8
bion-slmn <bionsol25@gmail.com>
2023-02-23 12:56:18 -0800

    square of #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_square - draws a square using # on the terminal
+ * @size: number of times the width and length to be printed
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++
+				
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
