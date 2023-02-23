4953f7bd029b7369a56b6a225da13ff9fc4b6f63
bion-slmn <bionsol25@gmail.com>
2023-02-23 13:55:33 -0800

    Fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry piont
+ * Return: Always 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d <= 100; d++)
+	{
+		if ((d % 5 == 0) && (d % 3 == 0))
+			printf("FizzBuzz ");
+
+		else if (d % 5 == 0)
+			printf("Buzz ");
+
+		else if
+			(d % 3 == 0)
+				printf("Fizz ");
+
+		else
+			printf("%d ", d);
+	}
+	return (0);
+}
