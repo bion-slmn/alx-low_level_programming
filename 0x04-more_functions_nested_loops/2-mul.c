278ffedc51d4a4b1c551be25a0dfefb01c7a6be1
bion-slmn <bionsol25@gmail.com>
2023-02-23 04:33:12 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies tow digits.
+ * @a: The first integer to be multiplied
+ * @b: are the digits.
+ *
+ * Return: is the answer.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
