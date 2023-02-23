4dbc8c0b3f0be693c332d91ab240793c5fd9594d
bion-slmn <bionsol25@gmail.com>
2023-02-23 13:06:55 -0800

    midnight

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,12 +8,17 @@ void print_square(int size)
 {
 	int x, y;
 
+	if (size > 0)
+	{
 	for (x = 0; x < size; x++)
 	{
-		for (y = 0; y < size; y++
-				
+		for (y = 0; y < size; y++)
+
 			_putchar('#');
 
 		_putchar('\n');
 	}
+	}
+	else
+		_putchar('\n');
 }
