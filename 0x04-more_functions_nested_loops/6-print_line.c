b600906e8c9f8f9e95e04280b8cedca0227d2c97
bion-slmn <bionsol25@gmail.com>
2023-02-23 08:01:44 -0800

    draw a stright lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+	_putchar('\n');
+	}
+}
