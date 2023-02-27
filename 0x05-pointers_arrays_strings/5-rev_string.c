93314be602008c3acecaeb3a6aab0a45d69a7f38
bion-slmn <bionsol25@gmail.com>
2023-02-27 11:11:58 -0800

    do hard things

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: is the pointer of the address
+ */
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char aux;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len--)
+	{
+		aux = s[i];
+		s[i++] = s[len];
+		s[len] = aux;
+	}
+}
