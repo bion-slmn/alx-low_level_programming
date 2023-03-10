6faffc0586e4de7dd15ddc4bea030e4bfe8e0bbf
bion-slmn <bionsol25@gmail.com>
2023-03-10 11:14:17 -0800

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - is the start of the program.
+ * @argc: counts the number of strings
+ * @argv: pointer to strings
+ * Return: an iteger when finished
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 1)
+		printf("Error\n");
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	return (0);
+}
