7f80f9d63b9c09cff29134e4d0d19900f2ff79b2
bion-slmn <bionsol25@gmail.com>
2023-03-10 11:26:52 -0800

    multiplicatn

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[])
 {
 	int mul;
 
-	if (argc == 1)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	if (argc == 3)
+	else
 	{
 		mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
