8bbfe741614ab8cbcfc64a754ae9526ac8a378c0
bion-slmn <bionsol25@gmail.com>
2023-03-14 04:36:07 -0700

    malloc 2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,21 +16,31 @@ int **alloc_grid(int width, int height)
 	if (height <= 0 || width <= 0)
 		return (NULL);
 
-	arr = malloc(height * sizeof(int*));
-
-	for (i = 0; i < height; i++)
-		arr[i] = malloc(width * sizeof(int));
+	arr = malloc(height * sizeof(int *));
 
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
+		arr[i] = malloc(width * sizeof(int));
+
+		if (arr[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+				free(arr[i]);
+
+			free(arr);
+			return (NULL);
+		}
+	}
+
+
+	for (i = 0; i < height; i++)
+	{
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 	}
-	return (arr);
 
-	for (i = 0; i < height; i++)
-		free(arr[i]);
+	return (arr);
 }
