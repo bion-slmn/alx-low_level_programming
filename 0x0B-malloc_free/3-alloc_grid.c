f1c740089e39f4f15b50bc03b941243897196478
bion-slmn <bionsol25@gmail.com>
2023-03-14 03:47:37 -0700

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocate memory to 2d array(grid).
+ * @width: is the width of grid.
+ * @height: is lenght of the grid.
+ *
+ * Return: an integer or NULL if fails
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arr;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	arr = malloc(height * sizeof(int));
+
+	for (i = 0; i < height; i++)
+		arr[i] = malloc(width * sizeof(int));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}
