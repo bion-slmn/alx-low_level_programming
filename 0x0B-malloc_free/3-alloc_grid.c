0957b1f0e917c039da26cc455b7e83df5b77b603
bion-slmn <bionsol25@gmail.com>
2023-03-14 04:02:54 -0700

    If you even dream of beatin

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (height <= 0 || width <= 0)
 		return (NULL);
 
-	arr = malloc(height * sizeof(int));
+	arr = malloc(height * sizeof(int*));
 
 	for (i = 0; i < height; i++)
 		arr[i] = malloc(width * sizeof(int));
@@ -30,4 +30,7 @@ int **alloc_grid(int width, int height)
 			arr[i][j] = 0;
 	}
 	return (arr);
+
+	for (i = 0; i < height; i++)
+		free(arr[i]);
 }
