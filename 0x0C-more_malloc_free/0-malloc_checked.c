df6fc17efd4bb343df414694621dc46bb4bc9165
bion-slmn <bionsol25@gmail.com>
2023-03-16 07:04:05 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: is the digit to be allocated.
+ *
+ * Return: pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+
+	void *c = malloc(b);
+	
+	if (c == NULL)
+		exit(98);
+
+	return(c);
+}
+
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,8 @@
+This is projectis about dynamic memory allocation.
+The functions involved are:
+free()
+malloc()
+calloc()
+realloc()
+exit()
+For this project we are only using free, malloc and exit.
