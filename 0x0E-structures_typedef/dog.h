64ac7e07c23f00b84ccd827788edfced62390feb
bion-slmn <bionsol25@gmail.com>
2023-03-20 08:09:17 -0700

    i dontknow

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,12 @@
 #define DOG_H
 
 /**
- * struct dog - defines a dog
- * @name: is the name of dog
- * @age: gives the age of dog
- * @owner: gives the owners name
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
- * Description: This gives infrormation about 
- * a dog
+ * Description: Longer description
  */
 struct dog
 {
