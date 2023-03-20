8477787897043866b7ed1cedcdbdd015e0d377d3
bion-slmn <bionsol25@gmail.com>
2023-03-20 06:49:02 -0700

    poppy

---
+++ 0x0E-structures_typedef/REAME.md
@@ -0,0 +1,2 @@
+This is a project about structures in c
+and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a dog
+ * @name: is the name of dog
+ * @age: gives the age of dog
+ * @owner: gives the owners name
+ *
+ * Description: This gives infrormation about 
+ * a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
