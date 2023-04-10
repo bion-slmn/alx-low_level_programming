bd74de987fc6b58d80b4f6d42f1a1b3fc1873768
bion-slmn <bionsol25@gmail.com>
2023-04-10 16:20:46 -0700

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,48 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "main.h"
+
+/**
+ * strlength -length of string.
+ * @str: is the string.
+ *
+ * Return: length
+ */
+int strlength(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: is the name of file.
+ * @text_content: s the NULL terminated string to add at the end
+ *
+ * Return:1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, count;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	count = write(fd, text_content, strlength(text_content));
+	if (count == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
