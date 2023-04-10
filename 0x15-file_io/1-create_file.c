0614f215c301dec917fc9ba108dbd9a2fabf03b3
bion-slmn <bionsol25@gmail.com>
2023-04-10 14:25:31 -0700

    creat and give permisions

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stddef.h>
+#include<unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * strlength -length of string.
+ * @str: is the string.
+ *
+ * Return: length
+ */
+int strlength(char *str)
+{
+        int i = 0;
+
+        while (str[i] != '\0')
+        {
+                i++;
+        }
+        return (i);
+}
+
+/**
+ * create_file - creates a file.
+ * @filename: is the name of the file to create
+ * @text_context: s a NULL terminated string to write
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+        int fd, count;
+
+        fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
+        if (fd == -1)
+                return (-1);
+
+        if (text_content == NULL)
+                return (0);
+
+        count = write(fd, text_content, strlength(text_content));
+        if (count == -1)
+        {
+                close(fd);
+                return (-1);
+        }
+
+        close(fd);
+        return (1);
+}
