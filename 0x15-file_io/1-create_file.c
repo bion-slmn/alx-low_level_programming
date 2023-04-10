a59636372f059a03e1b570238f8618630ba304c2
bion-slmn <bionsol25@gmail.com>
2023-04-10 14:34:15 -0700

    creat and give permisionz

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,6 +32,9 @@ int strlength(char *str)
 int create_file(const char *filename, char *text_content)
 {
         int fd, count;
+	
+	if (filename == NULL)
+		return (-1);
 
         fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
         if (fd == -1)
@@ -43,7 +46,6 @@ int create_file(const char *filename, char *text_content)
         count = write(fd, text_content, strlength(text_content));
         if (count == -1)
         {
-                close(fd);
                 return (-1);
         }
 
