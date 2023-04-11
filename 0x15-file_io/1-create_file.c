f200ac9682b834350eec40134a74ccde881f747b
bion-slmn <bionsol25@gmail.com>
2023-04-11 00:29:22 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,41 +12,40 @@
  */
 int strlength(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i] != '\0')
-        {
-                i++;
-        }
-        return (i);
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
  * create_file - creates a file.
- * @filename: is the name of the file to create
- * @text_context: s a NULL terminated string to write
+ * @filename: is the name of the file to create.
+ * @text_content: is a NULL terminated string to write.
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-        int fd, count;
-	
+	int fd, count;
+
 	if (filename == NULL)
 		return (-1);
 
-        fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-        if (fd == -1)
-                return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
 	if (text_content)
 		count = write(fd, text_content, strlength(text_content));
 
-        if (count == -1)
-        {
-                return (-1);
-        }
+	if (count == -1)
+	{
+		return (-1);
+	}
 
 	close(fd);
 	return (1);
