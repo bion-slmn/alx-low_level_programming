92d6fd21272137d0dd195a752557cc15854f154f
bion-slmn <bionsol25@gmail.com>
2023-05-08 12:46:42 -0700

    closed file if write fails

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		nwrite = write(fd, text_content, len);
 		if (nwrite < 0)
+		{
+			close(fd);
 			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
