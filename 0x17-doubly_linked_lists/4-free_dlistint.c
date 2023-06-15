db89cab698fae46ec5e25675b48f94d278957937
bion-slmn <bionsol25@gmail.com>
2023-06-15 14:01:51 -0700

    free

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -9,10 +9,14 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp = head;
 
+	if (head == NULL)
+		return;
+
 	while (temp->next != NULL)
 	{
 		free(temp);
 		temp = temp->next;
 	}
 	free(temp);
+	head = NULL;
 }
