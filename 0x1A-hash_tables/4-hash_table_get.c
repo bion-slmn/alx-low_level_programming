513bdec594cf1a923ae882f61f0b46d44f410431
bion-slmn <bionsol25@gmail.com>
2023-07-07 23:56:09 -0700

    you need to have check

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,6 +16,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *data;
 
+	if (ht == NULL)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 
 	data = ht->array[index];
