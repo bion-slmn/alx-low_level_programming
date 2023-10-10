0aaa9d1ffd0ff8430e539b43898a8efb0ac43a8c
bion-slmn <bionsol25@gmail.com>
2023-10-10 09:07:11 -0700

    betty in the search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -31,9 +31,5 @@ listint_t *jump_list(listint_t *list, size_t size, int value);
 
 void free_list(listint_t *list);
 listint_t *create_list(int *array, size_t size);
-void print_list(const listint_t *list);/*
-void free_skiplist(skiplist_t *list);
-skiplist_t *create_skiplist(int *array, size_t size);
-void print_skiplist(const skiplist_t *list);
-void init_express(skiplist_t *list, size_t size);*/
+void print_list(const listint_t *list);
 #endif
